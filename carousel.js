c3c483d0a3362c1eef1830360120b23def09e7ea
Lelu <elias.lugones1@hotmail.com>
2024-11-14 17:01:04 -0300

    Add files via upload

---
+++ carousel.js
@@ -0,0 +1,6 @@
+const imageSets = [
+    ["lattafa-asad.jfif", "lattafa-asad-2.jpg", "lattafa-asad-3.jpg"],
+    ["lataffa-yara.jfif", "lataffa-yara-2.jpg", "lataffa-yara-3.jpg"],
+    ["latafa-khamrah.jfif", "latafa-khamrah-2.jpg", "latafa-khamrah-3.jpg"],
+    ["jean-paul-gluttier-scandal.jfif", "jean-paul-gluttier-scandal-2.jpg", "jean-paul-gluttier-scandal-3.jpg"],
+    ["paco-rabanne-olympea.jfif", "paco-rabanne-olympea-
